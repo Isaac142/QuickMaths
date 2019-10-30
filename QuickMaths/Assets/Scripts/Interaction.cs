0d266dcf8a41e6a55c9b77c57641024558b57acc
Isaac <embramaze@gmail.com>
2019-10-30 19:06:59 +1100

    Bullets Shooting / RayCast Knowing What hit

---
+++ QuickMaths/Assets/Scripts/Interaction.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour
+{
+    private bool visible;
+
+    public GameObject rayStart;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RayCastActivator();
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+        if (Physics.Raycast(rayStart.transform.position, -rayStart.transform.up, out hit, 100))
+        {
+            Debug.DrawLine(rayStart.transform.position, hit.point);
+        }
+    }
+
+    void RayCastActivator()
+    {
+
+        //raycast from weapon
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(rayStart.transform.position, -rayStart.transform.up, out hit, 100))
+            {
+                Debug.DrawLine(rayStart.transform.position, hit.point);
+                print(hit.collider.name);
+            }
+        }
+
+        //Old Raycast from middle of camera
+
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //    RaycastHit hit;
+
+        //    if (Physics.Raycast(ray, out hit, 100))
+        //    {
+        //        print(hit.collider.name);
+        //    }
+
+        //    Debug.DrawLine(ray.origin, hit.point);
+        //}
+    }
+}
---
+++ QuickMaths/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, Vector3.forward, out hit, 100.0f))
+            {
+                print("Found a: " + hit.collider.name);
+                hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            }
+
+            Debug.DrawRay(transform.position, Vector3.forward, Color.blue);
+        }
+
+    }
+}
